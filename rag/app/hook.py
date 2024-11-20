ef039cdf2896b7de42ae6d98142c2d7987589649
Grig Alex <alexengrigdev@gmail.com>
2024-11-20 20:56:50 +0300

    Remove hook

--- rag/app/hook.py
+++ rag/app/hook.py
@@ -4,7 +4,7 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
-from app import pooling, pipelines
+from app import pooling
 
 logger = logging.getLogger(__name__)
 
@@ -12,7 +12,6 @@ logger = logging.getLogger(__name__)
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     logger.info('Startup hook')
-    pipelines.do_reindex()
     thread = threading.Thread(target=pooling.do_pooling)
     thread.daemon = True  # Позволяет завершить поток при остановке приложения
     thread.start()
