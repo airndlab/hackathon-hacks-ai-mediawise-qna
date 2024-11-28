1e9fa9c0630e359802244226e6702f07a80a7337
Grig Alex <alexengrigdev@gmail.com>
2024-11-28 13:06:02 +0300

    Add POOLING_ENABLED

--- rag/app/hook.py
+++ rag/app/hook.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import threading
 from contextlib import asynccontextmanager
 
@@ -8,12 +9,19 @@ from app import pooling
 
 logger = logging.getLogger(__name__)
 
+POOLING_ENABLED = os.getenv("POOLING_ENABLED", "true").lower() == "true"
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     logger.info('Startup hook')
-    thread = threading.Thread(target=pooling.do_pooling)
-    thread.daemon = True  # Позволяет завершить поток при остановке приложения
-    thread.start()
+
+    if POOLING_ENABLED:
+        logger.info('Pooling is enabled by POOLING_ENABLED, starting the pooling thread...')
+        thread = threading.Thread(target=pooling.do_pooling)
+        thread.daemon = True  # Позволяет завершить поток при остановке приложения
+        thread.start()
+    else:
+        logger.info('Pooling is disabled by POOLING_ENABLED.')
+
     yield
     logger.info('Shutdown hook')
